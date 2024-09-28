c17bbc3121f187f5b39239418250702d1d167a7d
aayu0903 <134001135+aayu0903@users.noreply.github.com>
2024-09-28 22:14:27 +0530

    Create resume.java

---
+++ resume.java
@@ -0,0 +1,18 @@
+public class Resume {
+    private String template;
+    private String name;
+    private String email;
+    private String phone;
+
+    public Resume(String template, String name, String email, String phone) {
+        this.template = template;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+    }
+
+    public String generateResume() {
+        // TO DO: Implement logic to generate the resume content based on the template and user input
+        return "";
+    }
+}
